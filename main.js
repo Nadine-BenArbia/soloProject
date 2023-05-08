ab6e09cb51cddbeef72957daf5c94866662bf1e7
nina benarbia <nadinebenarbia@gmail.com>
2023-05-08 21:37:19 +0100

    css done

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
+//dropDown
+$("#shoppingCart").click(function(){
+    $(".dropdown").show()
+    console.log("this",$("#order"))
+})
+$("#closeDrop").click(function(){
+    $(".dropdown").hide()
+    console.log("this",$("#order"))
+})
+// personal info
 $("#order").click(function(){
     $(".formContainer").show()
     console.log("this",$("#order"))
 })
 
-$("#order").click(function(){
-    $("#closeIcon").hide()
+$("#closeIcon").click(function(){
+    $(".formContainer").hide()
+    console.log($("#closeIcon"))
 })
+// shopping cart 
+var allCookies = document.getElementsByClassName("cookie")
+console.log("coookie",{allCookies})
+
+allCookies.Onclick=function  (){
+   // cookisList.push({allCookies.name,allCookies.price})
+}
